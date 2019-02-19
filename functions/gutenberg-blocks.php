01a2c65cfb88c3033b1ab541636c93e845c877ac
halfg33k <johnsonam17@gmail.com>
2019-02-19 16:04:24 -0500

    dependencies and container block

---
+++ functions/gutenberg-blocks.php
@@ -0,0 +1,61 @@
+<?php
+
+///////////////////////////////////////////////////////////////////////////////
+// DEPENDENCIES                                                              //
+///////////////////////////////////////////////////////////////////////////////
+function load_dependencies(){
+    wp_register_script(
+        'dependencies-script',
+        get_template_directory_uri() . '/js/block-dependencies.js',
+        array( 'wp-blocks', 'wp-element', 'wp-editor', 'wp-components' )
+    );
+
+    wp_enqueue_script( 'dependencies-script' );
+}
+add_action( 'init', 'load_dependencies', 10, 0 );
+
+///////////////////////////////////////////////////////////////////////////////
+// CONTAINER                                                                 //
+///////////////////////////////////////////////////////////////////////////////
+function container_block(){
+    wp_register_script(
+        'container-script',
+        get_template_directory_uri() . '/js/block-container.js',
+        array( 'wp-blocks', 'wp-element', 'wp-editor', 'wp-components' )
+    );
+
+    register_block_type('childress/container', array(
+        'editor_script' => 'container-script',
+    ) );
+}
+add_action( 'init', 'container_block', 10, 0 );
+
+///////////////////////////////////////////////////////////////////////////////
+// HERO BOX                                                                  //
+///////////////////////////////////////////////////////////////////////////////
+// function hero_box_block(){
+//     wp_register_script(
+//         'hero-box-script',
+//         get_template_directory_uri() . '/js/block-hero-box.js',
+//         array( 'wp-blocks', 'wp-element', 'wp-editor', 'wp-components' )
+//     );
+
+//     wp_register_style(
+//         'hero-box-editor-style',
+//         get_template_directory_uri() . '/css/block-hero-box-editor-style.css',
+//         array( 'wp-edit-blocks' )
+//     );
+
+//     wp_register_style(
+//         'hero-box-style',
+//         get_template_directory_uri() . '/css/block-hero-box-style.css',
+//         array( 'wp-edit-blocks' )
+//     );
+
+//     register_block_type('childress/hero-box', array(
+//         'editor_script' => 'hero-box-script',
+//         'editor_style'  => 'hero-box-editor-style',
+//         'style'  => 'hero-box-style',
+//     ) );
+// }
+// add_action( 'init', 'hero_box_block', 10, 0 );
